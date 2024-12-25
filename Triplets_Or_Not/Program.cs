df5960e707ac08179a9058fc7d2116c186b8d8d0
harshbhatt2106 <harshbhatt932@gmail.com>
2024-12-25 10:26:36 +0530

    Find_Triplets

---
+++ Triplets_Or_Not/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triplets_Or_Not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 9;
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; i < arr.Length; j++)
+                {
+                    for (int k = j + 1; k < arr.Length; k++)
+                    {
+                        if (arr[i] + arr[j] + arr[k] == sum)
+                        {
+                            Console.WriteLine($"Triplet is {arr[i]}, {arr[j]}, {arr[k]}");
+                        }
+                    }
+                }
+            }
+            Console.ReadLine(); 
+
+        }
+    }
+}
---
+++ Triplets_Or_Not/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Triplets_Or_Not")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Triplets_Or_Not")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3c8d8d41-9a0c-42bb-b6c5-c912d56100fc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
