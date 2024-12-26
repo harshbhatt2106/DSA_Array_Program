f009b5f65de2012a084e8f1b82c891ab56f315db
harshbhatt2106 <harshbhatt932@gmail.com>
2024-12-26 15:05:25 +0530

    Chocolate_Distribution_Problem

---
+++ Chocolate_Distribution_Problem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chocolate_Distribution_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 7, 3, 2, 4, 9, 12, 56 } ;
+            Array.Sort(arr);
+            int number_of_Student = 5;
+
+            int min = int.MaxValue;
+            int cur = 0;
+            for (int i = 0; i < arr.Length-number_of_Student; i++)
+            {
+                if (arr[i + number_of_Student - 1] - arr[i] < min)
+                {
+                    min = arr[i + number_of_Student - 1] - arr[i];
+                }
+            }
+            Console.WriteLine(min);
+            Console.ReadLine();
+        }
+    }
+}
