4c9169f3531cd453eb720a75f9f1bed6fdb5ae12
harshbhatt2106 <harshbhatt932@gmail.com>
2024-12-27 14:58:05 +0530

    Smallest subarray with sum greater than a given value

---
+++ Smallest subarray with sum greater than a given value/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Smallest_subarray_with_sum_greater_than_a_given_value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 10, 5, 2, 7 };
+            int x = 51;
+            int sum = 0;
+            int count = 1;
+            int oldcount = int.MaxValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = arr[i];
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    sum = sum + arr[j];
+                    count++;
+                    if (sum > x)
+                    {
+                        if (count < oldcount)
+                        {
+                            oldcount = count;
+                        }
+                        break;
+                    }
+                }
+                count = 1;
+            }
+            if (oldcount == int.MaxValue)
+            {
+                oldcount = 0;
+            }
+            Console.WriteLine(oldcount);
+            Console.ReadLine();
+        }
+    }
+}
