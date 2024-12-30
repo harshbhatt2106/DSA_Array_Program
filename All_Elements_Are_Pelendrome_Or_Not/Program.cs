a95a139d2c92fbd5c855fe98e0fc8f446039730a
harshbhatt2106 <harshbhatt932@gmail.com>
2024-12-30 14:38:21 +0530

    All_Elements_Are_Pelendrome_Or_Not_Inarray

---
+++ All_Elements_Are_Pelendrome_Or_Not/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_Elements_Are_Pelendrome_Or_Not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 121, 131, 202 };
+
+            if (MyClass.IsArrayIspele(arr,arr.Length))
+            {
+                Console.WriteLine("Is pelendrome");
+            }
+            else
+            {
+                Console.WriteLine("Is not pelendrome");
+            }
+            Console.ReadLine();
+        }
+    }
+    public static class MyClass
+    {
+        public static bool IsArrayIspele(int [] array,int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                bool abs =  Ispelendrome(array[i]);
+                if(abs==false)
+                {
+                    return false;
+                }
+               
+            }
+            return true;
+        }
+        public static bool Ispelendrome(int N)
+        {
+            string str = "" + N;
+            int len = str.Length;
+            for (int i = 0; i < len / 2; i++)
+            {
+                if (str[i] != str[len - 1 - i])
+                    return false;
+            }
+            return true;
+        }
+    }
+
+}
---
+++ All_Elements_Are_Pelendrome_Or_Not/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("All_Elements_Are_Pelendrome_Or_Not")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("All_Elements_Are_Pelendrome_Or_Not")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c69c46a4-4fd4-4669-b509-2d1a786196d6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
