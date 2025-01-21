68ab6c992ec0971cd9b09b57d6f0334a8c4a15bf
harshbhatt2106 <harshbhatt932@gmail.com>
2025-01-21 10:29:02 +0530

    Changes of Printable statement

--- Sum_Equalpair/Sum_Equal_MatchPair.cs
+++ Sum_Equalpair/Sum_Equal_MatchPair.cs
@@ -6,31 +6,32 @@ namespace Sum_Equalpair
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter Number Of Array  ");
+            Console.Write("Enter Number Of element Array  ");
             int a = Convert.ToInt32(Console.ReadLine());
 
             int[] data = new int[a];
             for (int i = 0; i < a; i++)
             {
-                Console.Write("Enter Value Of array ");
+                Console.Write("Enter Value Of array:");
                 data[i] = Convert.ToInt32(Console.ReadLine());
             }
 
-            Console.WriteLine("Enter target Number ");
+            Console.Write("Enter target Number:");
             int target = Convert.ToInt32(Console.ReadLine());
             int sum = 0;
-
+            int temp;
             for (int i = 0; i < data.Length; i++)
             {
                 for (int j = i + 1; j < data.Length; j++)
                 {
-                    int temp = data[i] + data[j];
+                    temp = data[i] + data[j];
                     if (temp == target)
                     {
+                        Console.WriteLine("sum of : " + data[i] + " and " + data[j] + "==:" + target);
                         sum++;
                     }
                 }
-                //Console.WriteLine(sum);
+                temp = 0;
             }
             Console.WriteLine("Sum Is:{0}", sum);
             Console.ReadLine();
