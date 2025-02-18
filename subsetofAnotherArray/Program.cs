3f7e39ace8b400c222273aadad2314224b85c6d9
Harshbhatt <113496500+harshbhatt2106@users.noreply.github.com>
2025-02-18 11:35:26 +0530

    Update Program.cs

--- subsetofAnotherArray/Program.cs
+++ subsetofAnotherArray/Program.cs
@@ -11,27 +11,25 @@ namespace subsetofAnotherArray
         static void Main(string[] args)
         {
             int[] first = { 10, 5, 2, 23, 19 };
-            int[] second = { 19, 5, 2 };
+            int[] second = {10,23,23};
 
             bool IsSubArray = false;
 
             for (int i = 0; i < second.Length; i++)
             {
+                IsSubArray = false;
                 for (int j = 0; j < first.Length; j++)
                 {
                     if (second[i] == first[j])
                     {
                         IsSubArray = true;
-                        break;
-                    }
-                    else
-                    {
-                        IsSubArray = false;
                     }
                 }
+                if(!IsSubArray)
+                {
+                    break;
+                }
             }
-
-
             if (IsSubArray)
             {
                 Console.WriteLine("Is Sub array");
